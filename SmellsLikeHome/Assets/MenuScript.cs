6421ed30d61546902adbf78ca6075049e4dbe692
Momapixelcre8 <47049117+Momapixelcre8@users.noreply.github.com>
2019-01-26 15:24:36 -0500

    MenuButton

---
+++ SmellsLikeHome/Assets/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   
+    public void ChangeScene(string SceneName)
+    {
+
+        SceneManager.LoadScene("MaisonPlayerV1", LoadSceneMode.Additive);
+      
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
