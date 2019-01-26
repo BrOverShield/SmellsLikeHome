e35f7288b40de499a8fce05e6377ccc4935d95e5
Vincent5885 <47049080+Vincent5885@users.noreply.github.com>
2019-01-26 00:11:05 -0500

    vince
    gggg

--- SmellsLikeHome/Assets/ScriptVince/Throw.cs
+++ SmellsLikeHome/Assets/ScriptVince/Throw.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
+    
+    public Transform player;
+    public Transform PlayerCam;
+    public float throwForce = 10;
+    bool hasPlayer = false;
+    bool beingCarried = false;
+    private bool touched = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,15 @@ public class Throw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if(dist <= 2.5f)
+        {
+            hasPlayer = true;
+        }
+        else{
+            hasPlayer = false;
+        }
         
+        if ( hasPlayer && Input.GetButtonDown("Use"))
     }
 }
