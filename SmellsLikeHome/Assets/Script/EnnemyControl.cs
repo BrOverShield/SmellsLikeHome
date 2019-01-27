341a3cf6cda1f1b9d6ca15f31ac98fe45d6f2bd9
JayMartin95 <jeremiemartin1995@gmail.com>
2019-01-27 03:03:45 -0500

    Delay door + window + Bug perso

--- SmellsLikeHome/Assets/Script/EnnemyControl.cs
+++ SmellsLikeHome/Assets/Script/EnnemyControl.cs
@@ -9,6 +9,8 @@ public class EnnemyControl : MonoBehaviour
     NavMeshAgent EnnemyNavMesh;
     public DoorInteraction doorScript;
     public WindowInteraction windowScript;
+    public GameObject objectAnimator;
+    private Animator animEnemy;
     public float speedNeighbor = 1f;
     private bool IsDeath = false;
     private float TimerDelete = 1f;
@@ -22,6 +24,8 @@ public class EnnemyControl : MonoBehaviour
     void Start()
     {
         EnnemyNavMesh = this.GetComponent<NavMeshAgent>();
+        animEnemy = objectAnimator.GetComponent<Animator>();
+        transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
     }
 
     // Update is called once per frame
@@ -39,6 +43,9 @@ public class EnnemyControl : MonoBehaviour
         if (IsDeath)
         {
             speedNeighbor = 0f;
+            animEnemy.enabled=false;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().useGravity = true;
             if (TimerDelete < 5f)
             {
                 TimerDelete += Time.deltaTime;
@@ -46,6 +53,9 @@ public class EnnemyControl : MonoBehaviour
             else
             {
                 Destroy(gameObject);
+                if (EnnemyNavMesh.remainingDistance <= 4f) {
+                    animEnemy.SetBool("IsReady", true);
+                }
             }
         }
     }
@@ -64,7 +74,8 @@ public class EnnemyControl : MonoBehaviour
         {
             //Pie lose health
             Debug.Log("Miam!");
-            Destroy(this.gameObject);
+            animEnemy.SetBool("IsEating", true);
+            StartCoroutine(DieTimer(2f));
         }
         if (other.tag == "Object")
         {
@@ -83,5 +94,9 @@ public class EnnemyControl : MonoBehaviour
             windowScript = other.GetComponent<WindowInteraction>();
             windowScript.WindowOpen = true;
         }
+    }
+    private IEnumerator DieTimer(float deathDelay) {
+        yield return new WaitForSeconds(deathDelay);
+        Destroy(this.gameObject);
     }
 }
